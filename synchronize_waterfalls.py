5bca23450bea91af824dbddd3ba59960908e2603
PF <pierre-francois.gimenez@laas.fr>
2019-01-11 18:08:15 +0100

    Synchronize

--- synchronize_waterfalls.py
+++ synchronize_waterfalls.py
@@ -6,9 +6,11 @@ import os
 import numpy as np
 from preprocess import *
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 config = Config()
-prefix = config.get_config("section")
+prefix = "raspi-merged"
 waterfall_duration = config.get_config_eval("waterfall_duration")
 delta_t = int(waterfall_duration * 1.5) # un peu de marge pour la recherche
 waterfall_dimensions_time = config.get_config_eval("waterfall_dimensions")[0]
@@ -34,33 +36,62 @@ print(nb_waterfall_per_day)
 #         nb += l-1
 # print(summ/nb)
 
-
 # waterfalls : index 0 = rpi, index 1 = jour, index 2 = fichier
-for d in range(len(days)):
-    d = 1
-    waterfall_date = max([waterfalls_int[r][d][0] for r in range(nb_rpi)])+1000*43080
+for d in range(8,len(days)):
+    # on crée le dossier de résultat s'il n'existe pas
+    if not os.path.exists(os.path.join(prefix,days[d])):
+        os.makedirs(os.path.join(prefix,days[d]))
+    waterfall_date = max([waterfalls_int[r][d][0] for r in range(nb_rpi)])
+    print("day",d,"/",len(days))
     for w in range(nb_waterfall_per_day):
-        print(waterfall_date)
+#        print(waterfall_date)
         l = []
+        error = False
         for r in range(nb_rpi):
 #            print("RPI",r)
             files = [t for t in waterfalls_int[r][d] if t > waterfall_date - delta_t and t < waterfall_date + delta_t]
+            if not files:
+                error = True
+            else:
 #            print(files)
-            initial_date = files[0]
-            print(files)
-            files = [os.path.join(directories[r],days[d],str(f)) for f in files]
-            print(files)
-            val = np.concatenate(read_files(files))
+                initial_date = files[0]
+#            print(files)
+                files = [os.path.join(directories[r],days[d],str(f)) for f in files]
+#            print(files)
+                val = np.concatenate(read_files(files))
 #            print(val.shape)
-            begin = int((waterfall_date - initial_date) / temporal_step)
-            print(waterfall_date, initial_date, temporal_step)
-            print(begin)
-            l.append(val[begin:begin + waterfall_dimensions_time])
-        l = np.array(l)
-        print(l.shape)
-        fig = plt.figure()
-#        plt.imshow(np.concatenate(l,axis=1), cmap='hot', interpolation='nearest', aspect='auto')
-        plt.imshow(np.max(l,axis=0), cmap='hot', interpolation='nearest', aspect='auto')
-        plt.show()
+                begin = int((waterfall_date - initial_date) / temporal_step)
+#            print(waterfall_date, initial_date, temporal_step)
+#            print(begin)
+                val = val[begin:begin + waterfall_dimensions_time]
+                if val.shape[0] < waterfall_dimensions_time:
+                    error = True
+                else:
+                    l.append(val)
+        if error:
+            print("Incomplete last waterfall")
+        else:
+            l = np.array(l)
+            print(l.shape)
+            scaler = StandardScaler()
+            scaler.fit(l.reshape(-1,1))
+            l = scaler.transform(l.reshape(-1,1)).reshape(3,50,1500)
+            l_save = l
+            print(l.shape)
+            fig = plt.figure()
+#            plt.imshow(np.concatenate(l,axis=1), cmap='hot', interpolation='nearest', aspect='auto')
+            l = np.moveaxis(l,0,2)
+            print(l.shape)
+            pca = PCA(1, svd_solver="full")
+            l = pca.fit_transform(l.reshape(-1,3))
+            print(pca.components_)
+            print(l.shape)
+            l = l.reshape(50,1500)
+#            plt.imshow(l_save[0], cmap='hot', interpolation='nearest', aspect='auto')
+            plt.imshow(np.concatenate((np.concatenate(l_save,axis=1), l), axis=1), cmap='hot', interpolation='nearest', aspect='auto')
+            plt.show()
+            exit()
+            l = np.max(l,axis=0)
+#        print(l.shape)
+            l.tofile(os.path.join(prefix,days[d],str(waterfall_date)))
         waterfall_date += waterfall_duration
-        exit()
